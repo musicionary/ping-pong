beb50b2dc64d5725f981f38c9f20e8ccf3cfb246
Chip Carnes <chip.carnes27@gmail.com>
2016-06-10 10:44:25 -0700

    replace array indexes with ping/pong/pingpong using for loop and conditional branching

--- js/script.js
+++ js/script.js
@@ -3,25 +3,47 @@ $(document).ready(function(){
 Back End
 ========================================*/
 //variables
+var numberArray = [];
 var pingArray = [];
 var pingString = "";
+// var re = / /;
+var ping;
+var pong;
 
+//spec: create list of numbers up to the number provided by user
 //function to create list up to number provided by user
-function pingList(number) {
-  pingArray = [];
+function createArray(number) {
+  numberArray = [];
   for(i = 1; i <= number; i++) {
-    pingArray.push("<li>" + i.toString() + "</li>");
+    numberArray.push(i);
   }
-  console.log(pingArray);
-
-  pingString = pingArray.join("");
-  console.log(pingString);
+  console.log(numberArray);
 }
 
+function ping(array) {
+  for(j = 0; j < array.length; j++){
+    //spec: replace numbers divisible by 15 with the word pingpong
+    if(array[j] % 15 === 0) {
+      array[j] = "pingpong";
 
+    //spec: replace numbers divisible by 5 with the word pong
+  }else if(array[j] % 5 === 0){
+      array[j] = "pong";
 
+    //spec: replace numbers divisible by 3 with the word ping
+  }else if(array[j] % 3 === 0) {
+      array[j] = "ping";
+    }
+  }
+  console.log(numberArray);
+}
 
-
+// function
+// for(i = 1; i <= number; i++) {
+//   pingArray.push("<li>" + i.toString() + "</li>");
+// }
+// pingString = pingArray.join("");
+// console.log(pingString);
 
 
 
@@ -33,9 +55,10 @@ Front End
     pingArray = [];
     var input = parseInt($("#number").val());
 
-    pingList(input);
+    createArray(input);
+    ping(numberArray);
 
 
-    $("#ping-output").html(pingString);
+    // $("#ping-output").html(pingString);
   });
 });
