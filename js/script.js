53735a76cb6feefbb6341daea0d57d7fb49f03ab
Chip Carnes <chip.carnes27@gmail.com>
2016-06-10 09:41:28 -0700

    Prints numbers to page as list items

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+$(document).ready(function(){
 /*=======================================
 Back End
 ========================================*/
+//variables
+var pingArray = [];
+var pingString = "";
+
 //function to create list up to number provided by user
+function pingList(number) {
+  pingArray = [];
+  for(i = 1; i <= number; i++) {
+    pingArray.push("<li>" + i.toString() + "</li>");
+  }
+  console.log(pingArray);
+
+  pingString = pingArray.join("");
+  console.log(pingString);
+}
+
 
 
 
@@ -12,12 +28,14 @@ Back End
 /*=======================================
 Front End
 ========================================*/
-$(document).ready(function(){
   $("#ping-it").submit(function(event){
+    event.preventDefault();
+    pingArray = [];
     var input = parseInt($("#number").val());
 
+    pingList(input);
 
-    
-    $("#output").text();
+
+    $("#ping-output").html(pingString);
   });
 });
